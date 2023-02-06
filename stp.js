9116309123948da69aabd1049917380dca2f9fb4
Amir-zeb <55942880+Amir-zeb@users.noreply.github.com>
2023-02-06 23:45:20 +0500

    Update stp.js

--- stp.js
+++ stp.js
@@ -37,9 +37,11 @@ function startPause() {
         currentvalue.innerHTML = "Pause";
         start();
         document.getElementById('save').disabled = true;
+        document.getElementById('reset').disabled = true;
     } else if (currentvalue.innerHTML == "Pause") {
         currentvalue.innerHTML = "Start";
         pause();
+        document.getElementById('reset').disabled = false;
         document.getElementById('save').disabled = false;
     }
 }
@@ -60,7 +62,6 @@ function save() {
     var setlap = document.getElementById("savetime");
     //Create a new list item when clicking on the "Add" button
     var li = document.createElement("li");
-    //var inputValue = document.write(min + " : " + sec + " : " + msec);
     var getlap = document.createTextNode(min + " : " + sec + " : " + msec);
     var a = document.createElement("a")
     a.setAttribute("type", "button")
@@ -75,59 +76,3 @@ const removeLi = (e) => {
     var node = e.parentElement.remove()
     return node
 }
-
-
-
-
-
-
-
-// var myNodelist = document.getElementsByTagName("LI");
-// var i;
-// for (i = 0; i < myNodelist.length; i++) {
-//   var span = document.createElement("SPAN");
-//   var txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   myNodelist[i].appendChild(span);
-// }
-
-// // Click on a close button to hide the current list item
-// var close = document.getElementsByClassName("close");
-// var i;
-// for (i = 0; i < close.length; i++) {
-//   close[i].onclick = function() {
-//     var div = this.parentElement;
-//     div.style.display = "none";
-//   }
-// }
-
-// // Add a "checked" symbol when clicking on a list item
-// var list = document.querySelector('ul');
-// list.addEventListener('click', function(ev) {
-//   if (ev.target.tagName === 'LI') {
-//     ev.target.classList.toggle('checked');
-//   }
-// }, false);
-
-// // Create a new list item when clicking on the "Add" button
-// function newElement() {
-//   var li = document.createElement("li");
-//   var inputValue = document.write(min+" : "+sec+" : "+msec);
-//   var t = document.createTextNode(inputValue);
-//   li.appendChild(t);
-//   document.getElementById("myUL").appendChild(li)
-
-//   var span = document.createElement("SPAN");
-//   var txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   li.appendChild(span);
-
-//   for (i = 0; i < close.length; i++) {
-//     close[i].onclick = function() {
-//       var div = this.parentElement;
-//       div.style.display = "none";
-//     }
-//   }
-// }
